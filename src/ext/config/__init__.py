bfced0c01b3e61b36d9929410076f31bedd7d939
Matheus Cogo <matheuscarvalhocogo@gmail.com>
2022-11-07 23:34:55 -0300

    ajustando configs;

--- src/ext/config/__init__.py
+++ src/ext/config/__init__.py
@@ -6,7 +6,7 @@ def init_app(app):
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
     app.config['CORS_HEADERS'] = "Content-Type"
     # app.config['CORS_RESOURCES'] = {r"/api/v1/*": {"origins": "http://localhost:3000/*"}}
-    app.config['CORS_RESOURCES'] = {r"/api/v1/*": {"origins": "http://http://matheuscogo.pythonanywhere.com//*"}}
+    app.config['CORS_RESOURCES'] = {r"/api/v1/*": {"origins": "http://http://matheuscogo.pythonanywhere.com/*"}}
     # app.config['CORS_RESOURCES'] = {r"/api/v1/*": {"origins": "http://localhost:3000/*"}}
     # app.config['CORS_RESOURCES'] = {r"/api/v1/*": {"origins": "http://192.168.5.1:3000/*"}}
     app.config["CORS_ALLOW_HEADERS"] = "*"
