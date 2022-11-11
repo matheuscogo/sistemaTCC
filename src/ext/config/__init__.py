d8cf416eb808531b92240b98d8d25ac7544dbc36
Matheus Cogo <matheuscarvalhocogo@gmail.com>
2022-11-11 01:04:29 -0300

    ajustando cors;

--- src/ext/config/__init__.py
+++ src/ext/config/__init__.py
@@ -5,8 +5,8 @@ def init_app(app):
     app.config["SQLALCHEMY_BINDS"] = False
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
     app.config['CORS_HEADERS'] = "Content-Type"
-    app.config['CORS_RESOURCES'] = {r"/api/v1/*": {"origins": "http://localhost:3000/*"}}
-    # app.config['CORS_RESOURCES'] = {r"/api/v1/*": {"origins": "https://matheuscogo.pythonanywhere.com/*"}}
+    # app.config['CORS_RESOURCES'] = {r"/api/v1/*": {"origins": "http://localhost:3000/*"}}
+    app.config['CORS_RESOURCES'] = {r"/api/v1/*": {"origins": "https://matheuscogo.pythonanywhere.com/*"}}
     # app.config['CORS_RESOURCES'] = {r"/api/v1/*": {"origins": "http://192.168.5.1:3000/*"}}
     app.config["CORS_ALLOW_HEADERS"] = "*"
     app.config["CORS_METHODS"] = ['GET', 'POST', 'DELETE', 'PUT', 'OPTIONS', 'HEAD']
