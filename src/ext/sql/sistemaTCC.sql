e4314c580cd6edc889b8075bef80bb239befc698
Matheus Cogo <matheuscarvalhocogo@gmail.com>
2022-12-05 23:16:08 -0300

    removendo sql e alterando na pasta certa;

--- src/ext/sql/sistemaTCC.sql
+++ src/ext/sql/sistemaTCC.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS matrizes  (
-  id SERIAL NOT NULL,
+  id INTEGER,
   rfid VARCHAR,
   numero VARCHAR,
   ciclos INTEGER,
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS matrizes  (
 );
 
 CREATE TABLE IF NOT EXISTS planos  (
-  id SERIAL NOT NULL,
+  id INTEGER,
   nome VARCHAR,
   descricao VARCHAR,
   tipo VARCHAR,
@@ -19,56 +19,72 @@ CREATE TABLE IF NOT EXISTS planos  (
 );
 
 CREATE TABLE IF NOT EXISTS dias  (
-  id SERIAL NOT NULL,
-  "planoId" BIGINT,
+  id INTEGER,
+  plano_id BIGINT,
   dia INTEGER,
   quantidade INTEGER,
   PRIMARY KEY (id),
-  CONSTRAINT fk_planoId FOREIGN KEY("planoId") REFERENCES planos (id)
+  CONSTRAINT fk_planoId FOREIGN KEY("plano_id") REFERENCES planos (id)
 );
 
 CREATE TABLE IF NOT EXISTS confinamentos  (
-  id SERIAL NOT NULL,
-  "planoId" BIGINT,
-  "matrizId" BIGINT,
-  dataConfinamento timestamp,
+  id INTEGER,
+  plano_id BIGINT,
+  matriz_id BIGINT,
+  data_confinamento timestamp,
   active BOOLEAN,
+  deleted BOOLEAN,
   PRIMARY KEY (id),
-  CONSTRAINT fk_planoId FOREIGN KEY("planoId") REFERENCES planos (id),
-  CONSTRAINT fk_matrizId FOREIGN KEY("matrizId") REFERENCES matrizes (id)
+  CONSTRAINT fk_planoId FOREIGN KEY("plano_id") REFERENCES planos (id),
+  CONSTRAINT fk_matriz_id FOREIGN KEY("matriz_id") REFERENCES matrizes (id)
 );
 
 CREATE TABLE IF NOT EXISTS  registros  (
-  id SERIAL NOT NULL,
-  "matrizId" BIGINT,
-  dataEntrada timestamp,
-  dataSaida timestamp,
-  tempo INTEGER,
+  id INTEGER,
+  matriz_id BIGINT,
+  data_entrada timestamp,
+  data_saida timestamp,
   quantidade INTEGER,
   PRIMARY KEY (id),
-  CONSTRAINT fk_matrizId FOREIGN KEY("matrizId") REFERENCES matrizes (id)
+  CONSTRAINT fk_matriz_id FOREIGN KEY("matriz_id") REFERENCES matrizes (id)
 );
 
 CREATE TABLE IF NOT EXISTS  inseminacoes  (
-  id SERIAL NOT NULL,
-  "planoId" BIGINT,
-  "matrizId" BIGINT,
-  "confinamentoId" BIGINT,
-  dataInseminacao timestamp,
+  id INTEGER,
+  plano_id BIGINT,
+  matriz_id BIGINT,
+  confinamento_id BIGINT,
+  data_inseminacao timestamp,
   active BOOLEAN,
+  deleted BOOLEAN,
   PRIMARY KEY (id),
-  CONSTRAINT fk_planoId FOREIGN KEY("planoId") REFERENCES planos (id),
-  CONSTRAINT fk_matrizId FOREIGN KEY("matrizId") REFERENCES matrizes (id),
-  CONSTRAINT fk_confinamentoId FOREIGN KEY("confinamentoId") REFERENCES confinamentos (id)
+  CONSTRAINT fk_planoId FOREIGN KEY("plano_id") REFERENCES planos (id),
+  CONSTRAINT fk_matriz_id FOREIGN KEY("matriz_id") REFERENCES matrizes (id),
+  CONSTRAINT fk_confinamento_id FOREIGN KEY("confinamento_id") REFERENCES confinamentos (id)
 );
 
 CREATE TABLE IF NOT EXISTS avisos (
-  id SERIAL NOT NULL,
-  "confinamentoId" BIGINT,
-  dataAviso timestamp,
-  separar BOOLEAN,
-  status BOOLEAN,
+  id INTEGER,
+  confinamento_id BIGINT NULL,
+  data_aviso timestamp,
+  separate BOOLEAN,
+  type INTEGER,
   active BOOLEAN,
+  deleted BOOLEAN,
   PRIMARY KEY (id),
-  CONSTRAINT fk_confinamentoId FOREIGN KEY("confinamentoId") REFERENCES confinamentos (id)
+  CONSTRAINT fk_confinamento_id FOREIGN KEY("confinamento_id") REFERENCES confinamentos (id)
+);
+
+CREATE TABLE IF NOT EXISTS alimentador  (
+  id INTEGER NOT NULL,
+  matriz_id INTEGER,
+  confinamento_id INTEGER,
+  plano_id INTEGER,
+  dataEntrada DATETIME,
+  quantidade INTEGER,
+  hash VARCHAR,
+  PRIMARY KEY (id),
+  FOREIGN KEY(matriz_id) REFERENCES matrizes (id),
+  FOREIGN KEY(confinamento_id) REFERENCES confinamentos (id),
+  FOREIGN KEY(plano_id) REFERENCES planos (id)
 );
